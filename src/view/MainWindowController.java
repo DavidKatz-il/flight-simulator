68bfe0c54a94da0b54c0810b9aee517b35cd828e
David Katz <41651296+DavidKatz-il@users.noreply.github.com>
2021-01-17 00:57:40 +0200

    add MainWindowController.java

---
+++ src/view/MainWindowController.java
@@ -0,0 +1,16 @@
+package view;
+
+import javafx.fxml.Initializable;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+
+public class MainWindowController implements Initializable {
+
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+
+}
