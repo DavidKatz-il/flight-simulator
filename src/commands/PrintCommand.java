992973a52f70168e9435b7e83eee1724ecbf51dc
David Katz <41651296+DavidKatz-il@users.noreply.github.com>
2021-01-17 00:46:06 +0200

    print a variable

--- src/commands/PrintCommand.java
+++ src/commands/PrintCommand.java
@@ -1,6 +1,7 @@
 package commands;
 
 import expressions.Expression;
+import interpreter.Utilities;
 
 import java.util.List;
 
@@ -8,7 +9,10 @@ public class PrintCommand extends CommandBase{
 
     @Override
     public Expression execute(List<String> args) throws Exception {
-        args.forEach(System.out::println);
+        String ex = String.join("", args.subList(1, args.size()));
+        if (Utilities.isVarExist(ex))
+            ex = "" + Utilities.getVarSymbol(ex).calculate();
+        System.out.println(ex);
         return () -> 0;
     }
     public void checkArgs(List<String> args) throws Exception {
