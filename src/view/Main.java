2e5c5140c26a171d9401a6bae5f21585e9fd4c0b
David Katz <41651296+DavidKatz-il@users.noreply.github.com>
2021-01-17 23:19:36 +0200

    add mvvm logic

--- src/view/Main.java
+++ src/view/Main.java
@@ -5,16 +5,29 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
+import model.Model;
+import viewmodel.ViewModel;
 
 
 public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("MainWindow.fxml"));
+//        MyInterpreter.parseScript("resources/script.txt");
+        FXMLLoader fxl = new FXMLLoader(getClass().getResource("MainWindow.fxml"));
+        AnchorPane root = (AnchorPane)fxl.load();
         primaryStage.setTitle("Flight Simulator");
         primaryStage.setScene(new Scene(root, 1000, 500));
         primaryStage.show();
+
+        Model m = new Model();
+        ViewModel vm = new ViewModel(m);
+        m.addObserver(vm);
+
+        MainWindowController mwc = fxl.getController();
+        mwc.setViewModel(vm);
+        vm.addObserver(mwc);
     }
 
     public static void main(String[] args) {
